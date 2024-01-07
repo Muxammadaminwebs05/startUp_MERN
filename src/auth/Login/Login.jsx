01ecde039514e9fc81e05e4ace0183ef00bbff4e
Muhammadamin <150646677+Muxammadaminwebs05@users.noreply.github.com>
2024-01-07 06:18:21 -1100

    Update Login.jsx

--- src/auth/Login/Login.jsx
+++ src/auth/Login/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Layout from "../../components/Layout/Layout";
-import "./Login.scss";
+import "./Login.scss"; 
 
 const Login = () => {
   const [loading, setLoading] = useState(true);
